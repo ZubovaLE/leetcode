e76101ce6253c12cadb09ed2a0a13c48b10f1695
NikiforovaLE <n.lubovj@gmail.com>
2023-03-04 23:28:26 +0300

    Add DayOfTheWeek class and add tests for it

---
+++ src/main/java/org/nikiforova/solutions/easy/string/DayOfTheWeek.java
@@ -0,0 +1,17 @@
+package org.nikiforova.solutions.easy.string;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class DayOfTheWeek {
+    /**
+     * @param day - an integer representing a day
+     * @param month - an integer representing a month
+     * @param year - an integer representing a year
+     * @return the corresponding day of the week for the date
+     */
+    public static String dayOfTheWeek(int day, int month, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    }
+}
---
+++ src/test/java/org/nikiforova/solutions/easy/string/DayOfTheWeekTest.java
@@ -0,0 +1,17 @@
+package org.nikiforova.solutions.easy.string;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayOfTheWeekTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"31, 8, 2019, Saturday", "18, 7, 1999, Sunday", "15, 8, 1993, Sunday"})
+    @DisplayName("Test dayOfTheWeek with different parameters")
+    void dayOfTheWeek(int day, int month, int year, String expected) {
+        assertEquals(expected, DayOfTheWeek.dayOfTheWeek(day, month, year));
+    }
+}
